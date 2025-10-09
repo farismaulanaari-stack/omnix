92521e3820b5d333cdb4d5b5b5a7a41a3f6e5643
farrel irawan <irawanssfarrel@gmail.com>
2025-10-09 15:31:07 +0700

    feat: add InputGroup, Input, and Textarea components for enhanced form handling

--- src/app/contact-us/layout.tsx
+++ src/app/contact-us/layout.tsx
@@ -5,7 +5,7 @@ export default function ContactPageLayout({
 }>) {
     return (
         <section className="w-full h-screen">
-            <div className="container w-full h-full flex items-center justify-center">
+            <div className="container mx-auto w-full h-full flex items-center justify-center">
                 { children }
             </div>
         </section>
---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,31 @@
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardContent,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+export default function ContactPage() {
+  return (
+    <>
+      <Card className="w-1/2 h-4/5 bg-indigo-100 ring ring-indigo-600/20 shadow-none rounded-r-none"></Card>
+      <Card className="w-1/2 h-5/6 ring ring-black/20 shadow-none rounded-l-none py-0">
+        <form action="" className="w-full h-full py-6 flex flex-col justify-between gap-6">
+            <CardHeader className="w-full flex flex-col gap-3">
+                <CardTitle className="text-4xl font-bold w-full">Send Us a Message</CardTitle>
+                <CardDescription className="text-base font-medium text-black/50 w-full">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, natus iusto quas nostrum illum minus molestias nulla iure dolorem sed!</CardDescription>
+            </CardHeader>
+            <CardContent className="w-full h-full bg-orange-200"></CardContent>
+            <CardFooter className="w-full h-[8%]">
+                <Button type="submit" className="w-full h-full">
+                    <p className="text-base font-medium">send your message</p>
+                </Button>
+            </CardFooter>
+        </form>
+      </Card>
+    </>
+  );
+}
