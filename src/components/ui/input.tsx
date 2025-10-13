ba9a92a11e326022bce867aee6fd5342490d1806
farrel irawan <irawanssfarrel@gmail.com>
2025-10-13 15:49:03 +0700

    refactor: remove variant support from Input component for simplified styling

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,34 +2,15 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-import { cva, type VariantProps } from "class-variance-authority";
-
-const inputVariants = cva(
-  "border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-  {
-    variants: {
-      variant: {
-        dafultInput: "",
-        defaultOmnix: "",
-      },
-    },
-    defaultVariants: {
-      variant: "dafultInput",
-    },
-  }
-);
-
-function Input({
-  className,
-  type,
-  variant,
-  ...props
-}: React.ComponentProps<"input"> & VariantProps<typeof inputVariants>) {
+function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
     <input
       type={type}
       data-slot="input"
-      className={cn(inputVariants({ variant }), className)}
+      className={cn(
+        "h-9 w-full min-w-0 rounded-md bg-transparent px-3 py-1 text-base transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        className
+      )}
       {...props}
     />
   );
