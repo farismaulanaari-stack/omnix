8e077a38f641a14fd7850c33c5ca56b97b50908c
farrel irawan <irawanssfarrel@gmail.com>
2025-10-23 13:48:57 +0700

    feat: modify card UI from feature card component

--- src/pages/features-page.tsx
+++ src/pages/features-page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowUpRightIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function FeaturesPage() {
@@ -21,113 +22,95 @@ export default function FeaturesPage() {
           </p>
         </div>
         <div className="w-full min-h-[800px] grid grid-cols-7 grid-rows-6 gap-4">
-          <Card className="col-span-3 row-span-3 shadow-none p-3 flex flex-col gap-3">
-            <div className="w-full flex items-center justify-start">
-              <div className="w-[100px] h-[100px] bg-indigo-100 rounded-md"></div>
-            </div>
-            <div className="w-full flex flex-col gap-2">
-              <CardTitle className="text-xl font-bold">
-                Lorem ipsum dolor sit amet consectetur.
-              </CardTitle>
-              <CardDescription className="text-sm font-medium">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore suscipit cum minus possimus corrupti cumque similique aliquam at cupiditate? Voluptatum.
-              </CardDescription>
-            </div>
-            <div className="w-full mt-auto">
-              <Button variant={"default"} asChild className="w-full h-14">
-                <Link href={"#"}>more about this</Link>
-              </Button>
-            </div>
+          <Card className="col-span-3 row-span-3 shadow-none p-3 flex flex-col gap-3 relative">
+            <CardHeader className="rounded-md shadow-none ring ring-black/20 h-[250px]"></CardHeader>
+            <CardContent className="px-0">
+              <CardTitle className="text-lg font-bold">Lorem ipsum dolor sit amet.</CardTitle>
+              <CardDescription className="text-sm font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius ex dolorem consectetur perferendis quas!</CardDescription>
+            </CardContent>
+          <CardFooter className="w-full px-0 flex items-center gap-0">
+            <Button variant={"outline"} className="rounded-full px-6 py-0.5 h-8">
+              <p className="text-sm font-medium">more about this feature</p>
+            </Button>
+            <Button variant={"outline"} className="w-8 h-8 rounded-full px-0">
+              <ArrowUpRightIcon />
+            </Button>
+          </CardFooter>
           </Card>
-          <Card className="col-span-2 row-span-3 col-start-4 shadow-none p-3 flex flex-col gap-3">
-            <div className="w-full flex items-center justify-start">
-              <div className="w-[100px] h-[100px] bg-indigo-100 rounded-md"></div>
-            </div>
-            <div className="w-full flex flex-col gap-2">
-              <CardTitle className="text-xl font-bold">
-                Lorem ipsum dolor sit amet consectetur.
-              </CardTitle>
-              <CardDescription className="text-sm font-medium">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore suscipit cum minus possimus corrupti cumque similique aliquam at cupiditate? Voluptatum.
-              </CardDescription>
-            </div>
-            <div className="w-full mt-auto">
-              <Button variant={"default"} asChild className="w-full h-14">
-                <Link href={"#"}>more about this</Link>
-              </Button>
-            </div>
+          <Card className="col-span-2 row-span-3 col-start-4 shadow-none p-3 flex flex-col gap-3 relative">
+            <CardHeader className="rounded-md shadow-none ring ring-black/20 h-[250px]"></CardHeader>
+            <CardContent className="px-0">
+              <CardTitle className="text-lg font-bold">Lorem ipsum dolor sit amet.</CardTitle>
+              <CardDescription className="text-sm font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius ex dolorem consectetur perferendis quas!</CardDescription>
+            </CardContent>
+          <CardFooter className="w-full px-0 flex items-center gap-0">
+            <Button variant={"outline"} className="rounded-full px-6 py-0.5 h-8">
+              <p className="text-sm font-medium">more about this feature</p>
+            </Button>
+            <Button variant={"outline"} className="w-8 h-8 rounded-full px-0">
+              <ArrowUpRightIcon />
+            </Button>
+          </CardFooter>
           </Card>
-          <Card className="col-span-2 row-span-3 col-start-6 shadow-none p-3 flex flex-col gap-3">
-            <div className="w-full flex items-center justify-start">
-              <div className="w-[100px] h-[100px] bg-indigo-100 rounded-md"></div>
-            </div>
-            <div className="w-full flex flex-col gap-2">
-              <CardTitle className="text-xl font-bold">
-                Lorem ipsum dolor sit amet consectetur.
-              </CardTitle>
-              <CardDescription className="text-sm font-medium">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore suscipit cum minus possimus corrupti cumque similique aliquam at cupiditate? Voluptatum.
-              </CardDescription>
-            </div>
-            <div className="w-full mt-auto">
-              <Button variant={"default"} asChild className="w-full h-14">
-                <Link href={"#"}>more about this</Link>
-              </Button>
-            </div>
+          <Card className="col-span-2 row-span-3 col-start-6 shadow-none p-3 flex flex-col gap-3 relative">
+            <CardHeader className="rounded-md shadow-none ring ring-black/20 h-[250px]"></CardHeader>
+            <CardContent className="px-0">
+              <CardTitle className="text-lg font-bold">Lorem ipsum dolor sit amet.</CardTitle>
+              <CardDescription className="text-sm font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius ex dolorem consectetur perferendis quas!</CardDescription>
+            </CardContent>
+          <CardFooter className="w-full px-0 flex items-center gap-0">
+            <Button variant={"outline"} className="rounded-full px-6 py-0.5 h-8">
+              <p className="text-sm font-medium">more about this feature</p>
+            </Button>
+            <Button variant={"outline"} className="w-8 h-8 rounded-full px-0">
+              <ArrowUpRightIcon />
+            </Button>
+          </CardFooter>
           </Card>
-          <Card className="col-span-2 row-span-3 row-start-4 shadow-none p-3 flex flex-col gap-3">
-            <div className="w-full flex items-center justify-start">
-              <div className="w-[100px] h-[100px] bg-indigo-100 rounded-md"></div>
-            </div>
-            <div className="w-full flex flex-col gap-2">
-              <CardTitle className="text-xl font-bold">
-                Lorem ipsum dolor sit amet consectetur.
-              </CardTitle>
-              <CardDescription className="text-sm font-medium">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore suscipit cum minus possimus corrupti cumque similique aliquam at cupiditate? Voluptatum.
-              </CardDescription>
-            </div>
-            <div className="w-full mt-auto">
-              <Button variant={"default"} asChild className="w-full h-14">
-                <Link href={"#"}>more about this</Link>
-              </Button>
-            </div>
+          <Card className="col-span-2 row-span-3 row-start-4 shadow-none p-3 flex flex-col gap-3 relative">
+            <CardHeader className="rounded-md shadow-none ring ring-black/20 h-[250px]"></CardHeader>
+            <CardContent className="px-0">
+              <CardTitle className="text-lg font-bold">Lorem ipsum dolor sit amet.</CardTitle>
+              <CardDescription className="text-sm font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius ex dolorem consectetur perferendis quas!</CardDescription>
+            </CardContent>
+          <CardFooter className="w-full px-0 flex items-center gap-0">
+            <Button variant={"outline"} className="rounded-full px-6 py-0.5 h-8">
+              <p className="text-sm font-medium">more about this feature</p>
+            </Button>
+            <Button variant={"outline"} className="w-8 h-8 rounded-full px-0">
+              <ArrowUpRightIcon />
+            </Button>
+          </CardFooter>
           </Card>
-          <Card className="col-span-2 row-span-3 col-start-3 row-start-4 shadow-none p-3 flex flex-col gap-3">
-            <div className="w-full flex items-center justify-start">
-              <div className="w-[100px] h-[100px] bg-indigo-100 rounded-md"></div>
-            </div>
-            <div className="w-full flex flex-col gap-2">
-              <CardTitle className="text-xl font-bold">
-                Lorem ipsum dolor sit amet consectetur.
-              </CardTitle>
-              <CardDescription className="text-sm font-medium">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore suscipit cum minus possimus corrupti cumque similique aliquam at cupiditate? Voluptatum.
-              </CardDescription>
-            </div>
-            <div className="w-full mt-auto">
-              <Button variant={"default"} asChild className="w-full h-14">
-                <Link href={"#"}>more about this</Link>
-              </Button>
-            </div>
+          <Card className="col-span-2 row-span-3 col-start-3 row-start-4 shadow-none p-3 flex flex-col gap-3 relative">
+            <CardHeader className="rounded-md shadow-none ring ring-black/20 h-[250px]"></CardHeader>
+            <CardContent className="px-0">
+              <CardTitle className="text-lg font-bold">Lorem ipsum dolor sit amet.</CardTitle>
+              <CardDescription className="text-sm font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius ex dolorem consectetur perferendis quas!</CardDescription>
+            </CardContent>
+          <CardFooter className="w-full px-0 flex items-center gap-0">
+            <Button variant={"outline"} className="rounded-full px-6 py-0.5 h-8">
+              <p className="text-sm font-medium">more about this feature</p>
+            </Button>
+            <Button variant={"outline"} className="w-8 h-8 rounded-full px-0">
+              <ArrowUpRightIcon />
+            </Button>
+          </CardFooter>
           </Card>
-          <Card className="col-span-3 row-span-3 col-start-5 row-start-4 shadow-none p-3 flex flex-col gap-3">
-            <div className="w-full flex items-center justify-start">
-              <div className="w-[100px] h-[100px] bg-indigo-100 rounded-md"></div>
-            </div>
-            <div className="w-full flex flex-col gap-2">
-              <CardTitle className="text-xl font-bold">
-                Lorem ipsum dolor sit amet consectetur.
-              </CardTitle>
-              <CardDescription className="text-sm font-medium">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore suscipit cum minus possimus corrupti cumque similique aliquam at cupiditate? Voluptatum.
-              </CardDescription>
-            </div>
-            <div className="w-full mt-auto">
-              <Button variant={"default"} asChild className="w-full h-14">
-                <Link href={"#"}>more about this</Link>
-              </Button>
-            </div>
+          <Card className="col-span-3 row-span-3 col-start-5 row-start-4 shadow-none p-3 flex flex-col gap-3 relative">
+            <CardHeader className="rounded-md shadow-none ring ring-black/20 h-[250px]"></CardHeader>
+            <CardContent className="px-0">
+              <CardTitle className="text-lg font-bold">Lorem ipsum dolor sit amet.</CardTitle>
+              <CardDescription className="text-sm font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius ex dolorem consectetur perferendis quas!</CardDescription>
+            </CardContent>
+          <CardFooter className="w-full px-0 flex items-center gap-0">
+            <Button variant={"outline"} className="rounded-full px-6 py-0.5 h-8">
+              <p className="text-sm font-medium">more about this feature</p>
+            </Button>
+            <Button variant={"outline"} className="w-8 h-8 rounded-full px-0">
+              <ArrowUpRightIcon />
+            </Button>
+          </CardFooter>
           </Card>
         </div>
       </div>
