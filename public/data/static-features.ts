276e04e2caaa4c1c1f6f2af483fa747e559efad9
farrel irawan <irawanssfarrel@gmail.com>
2025-11-01 00:04:15 +0700

    feat: modify static features data to follow types on features card props interface

--- public/data/static-features.ts
+++ public/data/static-features.ts
@@ -1,46 +1,46 @@
 import { FeaturesCardProps } from "@/types/types";
 
 export const staticFeaturesData: FeaturesCardProps[] = [
-    {
-        image: "",
-        title: "Lorem ipsum dolor sit amet.",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
-        ctaHref: "#",
-        gridClassName: "col-span-3 row-span-3",
-    },
-    {
-        image: "",
-        title: "Lorem ipsum dolor sit amet.",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
-        ctaHref: "#",
-        gridClassName: "col-span-2 row-span-3 col-start-4",
-    },
-    {
-        image: "",
-        title: "Lorem ipsum dolor sit amet.",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
-        ctaHref: "#",
-        gridClassName: "col-span-2 row-span-3 col-start-6",
-    },
-    {
-        image: "",
-        title: "Lorem ipsum dolor sit amet.",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
-        ctaHref: "#",
-        gridClassName: "col-span-2 row-span-3 row-start-4",
-    },
-    {
-        image: "",
-        title: "Lorem ipsum dolor sit amet.",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
-        ctaHref: "#",
-        gridClassName: "col-span-2 row-span-3 col-start-3 row-start-4",
-    },
-    {
-        image: "",
-        title: "Lorem ipsum dolor sit amet.",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
-        ctaHref: "#",
-        gridClassName: "col-span-3 row-span-3 col-start-5 row-start-4",
-    },
-]
+  {
+    index: 1,
+    image: "",
+    title: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
+  },
+  {
+    index: 2,
+    image: "",
+    title: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
+  },
+  {
+    index: 3,
+    image: "",
+    title: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
+  },
+  {
+    index: 4,
+    image: "",
+    title: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
+  },
+  {
+    index: 5,
+    image: "",
+    title: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
+  },
+  {
+    index: 6,
+    image: "",
+    title: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eius exdolorem consectetur perferendis quas!",
+  },
+];
