140f3ab85e54e79a80f61df27a81851edd2640e1
farrel irawan <irawanssfarrel@gmail.com>
2025-11-18 19:40:37 +0700

    feat: implement validation rule and validation message to newsletter component

--- src/components/Newsletter/newsletter.tsx
+++ src/components/Newsletter/newsletter.tsx
@@ -12,11 +12,12 @@ export function NewsletterComponent({ className }: NewsLetterProps) {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
+    resetField,
   } = useForm<NewsLetterData>();
 
   const onSubmit: SubmitHandler<NewsLetterData> = (data) => {
-    console.log(data);
+    if (isValid) resetField("email_address");
   };
   return (
     <div
@@ -36,23 +37,37 @@ export function NewsletterComponent({ className }: NewsLetterProps) {
         </p>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-row items-center justify-start gap-3 w-fit"
+          className="flex flex-col items-start justify-center gap-3 w-fit"
         >
-          <Input
-            type="email"
-            className="bg-white placeholder:text-sm placeholder:font-medium focus-visible:border-indigo-100 focus-visible:ring-indigo-600 focus-visible:ring-[3px]"
-            placeholder="Enter your email address"
-            aria-invalid={errors.email_address?.message ? true : false}
-            {...register("email_address")}
-          />
-          <Button
-            variant={"default"}
-            size={"default"}
-            type="submit"
-            className="bg-indigo-600 text-white px-6 hover:bg-indigo-950 active:bg-indigo-800"
-          >
-            subscribe
-          </Button>
+          <div className="flex flex-row items-center justify-between gap-3 w-fit">
+            <Input
+              type="email"
+              className="bg-white placeholder:text-sm placeholder:font-medium focus-visible:border-indigo-100 focus-visible:ring-indigo-600 focus-visible:ring-[3px] aria-invalid:bg-red-100 aria-invalid:placeholder:text-red-600"
+              placeholder="Enter your email address"
+              aria-invalid={errors.email_address?.message ? true : false}
+              {...register("email_address", {
+                required: "email address is required",
+                minLength: {
+                  value: 5,
+                  message: "minimal length of email address is 5",
+                },
+              })}
+            />
+            <Button
+              variant={"default"}
+              size={"default"}
+              type="submit"
+              className="bg-indigo-600 text-white px-6 hover:bg-indigo-950 active:bg-indigo-800"
+            >
+              subscribe
+            </Button>
+          </div>
+          {/* validation message */}
+          {errors.email_address?.message && (
+            <p className="text-sm text-red-600">
+              {errors.email_address.message}
+            </p>
+          )}
         </form>
       </div>
     </div>
