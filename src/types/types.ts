57c37036b17ad497c933db0137fafaacb36b8fc7
farrel irawan <irawanssfarrel@gmail.com>
2025-11-24 02:16:34 +0700

    Revert: go back 1 commit before last commit

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,8 +2,6 @@ import * as SelectPrim from "@radix-ui/react-select";
 import { LucideIcon } from "lucide-react";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import {
-  ButtonHTMLAttributes,
-  FormHTMLAttributes,
   HTMLAttributes,
   InputHTMLAttributes,
   TextareaHTMLAttributes,
@@ -106,16 +104,4 @@ export interface NewsLetterProps extends HTMLAttributes<HTMLDivElement> {
 
 export interface NewsLetterData {
   email_address: string;
-}
-
-export interface FormProps extends FormHTMLAttributes<HTMLFormElement> {
-  children: React.ReactNode;
-  formButton: boolean;
-}
-
-export interface FormButtonProps
-  extends ButtonHTMLAttributes<HTMLButtonElement> {
-  textButton: string;
-  Icon?: LucideIcon;
-  loading?: boolean;
 }
