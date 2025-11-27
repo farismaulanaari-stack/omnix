457e6104d52565216d75d7a36646d966d530d51f
farrel irawan <irawanssfarrel@gmail.com>
2025-11-28 00:25:09 +0700

    feat: changing href data on static industry data

--- public/data/static-industry.ts
+++ public/data/static-industry.ts
@@ -3,32 +3,32 @@ import { IndustryCardProps } from "@/types/types";
 import { Cpu, Factory, ScanHeart, ReceiptText } from "lucide-react";
 
 export const staticIndustryData: IndustryCardProps[] = [
-    {
-        industry: "technology",
-        title: "Technology",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, officia.",
-        href: "#",
-        Icon: Cpu
-    },
-    {
-        industry: "manufacturing",
-        title: "Manufacturing",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, officia.",
-        href: "#",
-        Icon: Factory
-    },
-    {
-        industry: "healthcare",
-        title: "Healthcare",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, officia.",
-        href: "#",
-        Icon: ScanHeart
-    },
-    {
-        industry: "retail",
-        title: "Retail",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, officia.",
-        href: "#",
-        Icon: ReceiptText
-    },
-]
+  {
+    title: "Technology",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, officia.",
+    href: "/industry#technology",
+    Icon: Cpu,
+  },
+  {
+    title: "Manufacturing",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, officia.",
+    href: "/industry#manufacture",
+    Icon: Factory,
+  },
+  {
+    title: "Healthcare",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, officia.",
+    href: "/industry#healthcare",
+    Icon: ScanHeart,
+  },
+  {
+    title: "Retail",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, officia.",
+    href: "/industry#retail",
+    Icon: ReceiptText,
+  },
+];
