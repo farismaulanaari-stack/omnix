7f31d92641805bc159b1090972414e867292a277
farrel irawan <irawanssfarrel@gmail.com>
2025-11-28 02:22:53 +0700

    feat: adding solutions intro section

--- src/app/solutions/page.tsx
+++ src/app/solutions/page.tsx
@@ -1,5 +1,11 @@
 import SolutionsHeroSection from "@/pages/solutions-hero-section";
+import SolutionsIntroSection from "@/pages/solutions-intro-section";
 
 export default function SolutionsPage() {
-  return <SolutionsHeroSection />;
+  return (
+    <>
+      <SolutionsHeroSection />
+      <SolutionsIntroSection />
+    </>
+  );
 }
